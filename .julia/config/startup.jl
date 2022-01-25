4a142582fab19bad5838ac4ced3ce7341bbb5c78
Tomas Fiers <tomas.fiers@gmail.com>
2022-01-25 00:59:23 +0000

    add julia/config/startup.jl

---
+++ .julia/config/startup.jl
@@ -0,0 +1,13 @@
+# To avoid the below: `julia --startup-file=no`.
+
+using Pkg
+if isfile("Project.toml") || isfile("JuliaProject.toml")
+    Pkg.activate(".")
+end
+
+using OhMyREPL
+OhMyREPL.colorscheme!("OneDark")
+OhMyREPL.enable_autocomplete_brackets(true)
+OhMyREPL.Passes.RainbowBrackets.activate_256colors()
+
+using Revise
