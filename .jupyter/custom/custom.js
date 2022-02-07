b136be725087810c95df6200511d5ebdcb07e644
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-07 18:51:02 +0000

    jupyter custom.js: hide header at init

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -1,8 +1,36 @@
 
+window.setTimeout(on_init, 500)
+// Note: using `$([IPython.events]).on(x, function() {…})` with x =
+// "notebook_loaded.Notebook" or "app_initialized.DashboardApp" (all such events:
+// https://docs.jupyter.org/en/latest/contributing/ipython-dev-guide/js_events.html)
+// does not work. It does work in UserScript weirdly. Also tried using
+// `define(['base/js/namespace', 'base/js/events', 'base/js/promises',], function (IPython, events, promises) { … })`
+// and `events.on(` or `promises.app_initialized` (from https://github.com/jupyter/notebook/blob/master/notebook/static/custom/custom.js),
+// but no dice, very weirdly.
+
+function on_init() {
+    change_tree_sort_order()
+    hide_header()
+}
+
 // Sort notebooks by name *descending* by default.
 // When using ISO-8601-date-prefixed filenames, the newest are then on top.
 // See https://github.com/jupyter/notebook/issues/1267#issuecomment-1031481244
-function change_order(){
-    $("#sort-name").click();  // Default is sort ascending. Click to sort descending.
-};
-window.setTimeout(change_order, 500); // delay after DOM ready
+function change_tree_sort_order() {
+    $("#sort-name").click()  // Default is sort ascending. Click to sort descending.
+}
+
+function hide_header() {
+    Jupyter.keyboard_manager.actions._actions["hide_header:toggle"].handler()
+    // (Invocation found via looking in the hide-header nbextension src, and exploring the `Jupyter` obj in devtools console).
+}
+
+
+// More usefulness:
+//
+// - To find config and extension dirs: `jupyter --paths`
+//   Extensions are for me eg in C:\ProgramData\jupyter\nbextensions
+//
+// - Extension dev guide:
+//   https://jupyter-notebook.readthedocs.io/en/stable/extending/frontend_extensions.html
+//
