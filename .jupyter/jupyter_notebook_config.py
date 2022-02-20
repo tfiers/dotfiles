3b00d148ee91b69c94bcf86b270f060504e4e279
Tomas Fiers <tomas.fiers@gmail.com>
2022-02-20 16:29:35 +0000

    new: jupyter_notebook_config.py: open_browser False

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+# See here for available options: https://jupyter-notebook.readthedocs.io/en/stable/config.html
+# Or generate a file with defaults: `jup --generate-config`.
+# (No worries, it prompts before overwriting this file).
+
+# Some trickery here, apparently: we do not need to import any `c`; it is injected.
+
+c.NotebookApp.open_browser = False
