6729fe2c608a50937123a2499a27cb2438804e41
Tomas Fiers <tomas.fiers@gmail.com>
2022-11-20 18:49:58 +0000

    move `@sketch` functionality to a sketch

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -54,37 +54,19 @@ macro re()
     end
 end
 
-"""
-    @sketch "wack-idea"
-
-Create a new 'sketch', or open an existing one. A sketch is a julia script plus project: for
-when you want more than just the repl but less than a package.
-"""
-macro sketch(name)
-    print("Working on it … ")
-    @assert typeof(name) ∈ [String, Symbol]
-    :( _sketch($(string(name))) )
-end
-
-function _sketch(sketch_name::String)
-    sketches_dir = joinpath(homedir(), ".julia", "sketches")
-    mkpath(sketches_dir)
-    cd(sketches_dir)
-    edit(".")  # Open vs code in sketches root: git repo is there.
-    mkpath(sketch_name)
-    cd(sketch_name)
-    println("ok")
-    printstyled("  Changing", color = :green)
-    println(" current directory to, and")
-    Pkg.activate(".")  # "  Activating new project at …"
-    script = "main.jl"
-    touch(script)
-    edit(script)
-    include_str = "include(\"$script\")"
-    clipboard(include_str)  # To paste in repl
-    print("  `")
-    printstyled(include_str, color = :green)
-    println("` written to clipboard")
+macro sk()
+    # FIXME this doesn't work if Crayons not installed.
+    # should be proper newpkg.
+    quote
+        if isdefined(Main, :sketch)
+            is_first_call = false
+        else
+            print("Loading … ")  # This printed line is completed in `sketch()`.
+            is_first_call = true
+            include(homedir() * "/.julia/sketches/sketch-sel/main.jl")
+        end
+        sketch(is_first_call)
+    end
 end
 
 macro newpkg(name::Symbol)
