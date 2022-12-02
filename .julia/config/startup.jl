bb76a95bd24a7670a2568713bf8100e49ebe8033
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-02 03:50:07 +0000

    work on @newpkg

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -79,6 +79,7 @@ macro newpkg(name::Symbol)
             # ↪ Doing `@eval using PkgTemplates` in the function (`_newpkg`) does not work
             #   (`MethodError: no method matching License(; name="MIT")`).
         )
+        using DefaultApplication
         _newpkg($(string(name)))
     end
 end
@@ -86,34 +87,71 @@ end
 function _newpkg(name::String)
     @withfb "Instantiating Template" begin
         genpkg = Template(
-            julia = v"1.2",  # Required for `Tests(project = true)`
+            julia = v"1.2",  # v"1.2" is required for `Tests(project = true)`
             plugins = [
                 License(name = "MIT"),
                 Git(ssh = true),
                 Tests(project = true),
-                !CompatHelper,
-                !TagBot,
+                Documenter{GitHubActions}(),
+                # ↪ I want the unreleased as of now version by default..
+                #   eh well, do that below: `cd(docdir) do Pkg.add(url=…)`
+                #   OOrrr: don't use this Plugin. There's too much to tweak with it.
+                GitHubActions(
+                    coverage = false,
+                    extra_versions = [
+                        # "1.2",    # Aug 2019
+                        # "1.3",    # Nov 2019
+                        # "1.4",    # Mar 2020
+                        # "1.5",    # Aug 2020
+                        "1.6",      # Mar 2021. LTS
+                        "1.7",      # Nov 2021
+                        "1.8",      # Aug 2022. Stable
+                        "nightly",  # last commit to master
+                    ]
+                ),
+                RegisterAction(),
+                TagBot(),  # Makes git tag and gh Release on new General registry version.
+                           # also, _something_ builds the docs for each minor.
+                CompatHelper(),  # Runs every night, to… uh see if new versions released of deps?
+                Readme(destination = "ReadMe.md"),
+                # ↪ why not a custom template file, easy.
+                #   Also: https://juliaci.github.io/PkgTemplates.jl/stable/user/#Extending-Existing-Plugins-1
                 Develop(),
                 # ↪ Does `]dev NewPkg` in currently active env, so that we can run its code with
-                #   `using NewPkg` (after `using Revise`). Note that after the
-                #   `Pkg.activate(srcdir)` below, we're no longer in this currently active env.
+                #   `using NewPkg` (after `using Revise`). Note that after the `Pkg.activate(srcdir)`
+                #   below, we're no longer in this currently active env.
             ],
         )
     end
     println("Creating new package '$name'")
+    println("<PkgTemplates.jl output>")
     genpkg(name)
+    println("</PkgTemplates.jl output>\n")
     srcdir = joinpath(Pkg.devdir(), name)
     println("Changing active directory to ", srcdir)
     cd(srcdir)
     edit(".")
     Pkg.activate(".")
     println()
-    println("Creating gh repo")
-    reponame = name * ".jl"
-    run(`gh repo create --public $reponame`)
-    println()
-    println("Pushing to repo:")
-    run(`git push --set-upstream origin main`)  # `origin` auto-set by PkgTemplates
-    println()
-    println(" ↪ https://github.com/tfiers/$reponame")
+    choice = Base.prompt("Create github repo? [y]/n")
+    if choice != "n"
+        println("Creating gh repo")
+        reponame = name * ".jl"
+        run(`gh repo create --public $reponame`)
+        println()
+        println("Pushing to repo:")
+        run(`git push --set-upstream origin main`)  # `origin` auto-set by PkgTemplates
+        println()
+        DefaultApplication.open("https://github.com/tfiers/$reponame")
+        println("[reminder to go to repo settings: gh pages branch, and DOCUMENTER_URL]")
+        # There's `gh secret set` :)
+        # Doesn't seem to be sth for gh pages setting.
+        # But there's `gh browse -s` (open browser to settings).
+        # or well yeah just `https://github.com/tfiers/PkgDepGraph.jl/settings/pages`
+        # Weird though. there's not even a 'https://cli.github.com/manual/gh_extension' for it.
+        # oohh, can disable projects: `gh repo edit --enable-projects=false`
+        #
+        # Ah but wait!  Maybe this `julia-actions/julia-docdeploy@v1` is outdated.
+        # like, i think there's some gh-pages action for which you don't need to go set branch. no?
+    end
 end
